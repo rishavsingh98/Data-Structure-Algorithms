628ad86e914faa277f3ec138f15945c6ff13c738
Rishav Singh <45991488+rishavsingh98@users.noreply.github.com>
2022-07-27 13:38:16 +0530

    Added Recursive Solution

--- Binary Trees/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ Binary Trees/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -16,6 +16,8 @@ Example 3:
 Input: root = [1,2], p = 1, q = 2
 Output: 1
 */
+
+//Iterative Post Order Traversal Solution
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -72,5 +74,31 @@ public:
             
         }
         return pPar;
+    }
+};
+
+//Recursive Solution
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        if (!root)
+            return root;
+        
+		// If p or q if found, return the root.
+        if (root == p || root == q)
+            return root;
+        
+		// Save the root found at left and right recursively
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        
+		// If both the nodes are found, return parent of that node. That will be root.
+        if (left && right)
+            return root;
+        // If left is found, and right is not found.
+		// It is given that both the p and q will exist for sure.
+		// If left present then right will be a decendent of left, or vice versa.
+        return left? left: right;
     }
 };
